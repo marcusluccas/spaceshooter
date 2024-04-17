e1dc8250e670f5af12f1045b43fcb632d81e0f47
marcusluccas <lucilenebarbosamoura@gmail.com>
2024-04-17 18:57:07 -0300

    Fazendo o boss se mover

--- Spaceshooter/Assets/Scripts/BossController.cs
+++ Spaceshooter/Assets/Scripts/BossController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    private string estado = "estado1";
+    private float velocidade = 3;
+    private Rigidbody2D meuRB;
+    private float limiteX = 6f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meuRB = GetComponent<Rigidbody2D>();
+        meuRB.velocity = new Vector2(velocidade, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (estado)
+        {
+            case "estado1":
+                Estado1();
+                break;
+        }
+    }
+
+    private void Estado1()
+    {
+        if (transform.position.x > limiteX)
+        {
+            meuRB.velocity = new Vector2(-velocidade, 0f);
+        }
+        if (transform.position.x < -limiteX)
+        {
+            meuRB.velocity = new Vector2(velocidade, 0f);
+        }
     }
 }
