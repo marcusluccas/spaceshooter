7ea6c6e0f8c7b26f3f31fc8857c475daf54d60a1
marcusluccas <lucilenebarbosamoura@gmail.com>
2024-04-20 09:46:07 -0300

    Fazer os botões funcionarem feito

---
+++ Spaceshooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CarregarJogo()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void VoltaMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
